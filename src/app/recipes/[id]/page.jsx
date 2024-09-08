2902a741db2c987fec7070b647995cb873354a55
dalton.harenza <dalton.harenza@atlanticventurepartners.tech>
2024-09-08 14:36:13 -0700

    add form to recipe show

--- src/app/recipes/[id]/page.jsx
+++ src/app/recipes/[id]/page.jsx
@@ -1,11 +1,11 @@
 "use client"
 
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const Recipe = ({ params }) => {
     const [loading, setLoading] = useState(true);
     const [recipe, setRecipe] = useState();
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,16 +24,41 @@ const Recipe = ({ params }) => {
         fetchData().catch(console.error);
     }, []);
 
+    function submitForm(formData) {
+        const payload = {
+            name: formData.get('name'),
+            ingredients: formData.getAll('ingredients')
+        };
+
+        fetch(`/menumagic/api/v1/recipes/${params.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            if (response.ok) {
+                alert('Recipe updated successfully');
+            } else {
+                alert('Failed to update recipe');
+            }
+        }).catch(console.error);
+    }
+
     return (
         loading ? <div>Loading...</div> :
-            <div className="flex-1 flex flex-col bg-white">
-                <div className="text-4xl justify-center my-2">{recipe.name}</div>
+            <form action={submitForm} className="flex flex-col">
+                <div class="flex justify-center">
+                    <input type="text" name="name" className="text-4xl text-center align-self-center w-fit" defaultValue={recipe.name} />
+                </div>
+                <h2 className="mt-4">Ingredients</h2>
                 <div id="recipe" className="flex-col gap-2">
                     {recipe.ingredients?.map((ingredient) => (
                         <div key={ingredient.id} className="text-xl font-bold">{ingredient.name} - {ingredient.quantity} {ingredient.unit}</div>
                     ))}
                 </div>
-            </div>
+                <button type="submit">Update Recipe</button>
+            </form>
     );
 
 };
